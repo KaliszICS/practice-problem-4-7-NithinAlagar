219326378f1a066242cb41b2487d80808f91d07d
Nithin Alagar <343954178@tdsb.ca>
2026-04-22 13:59:05 -0400

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,28 @@
+/**
+	* Lesson: Random Module
+	* Author: Mr. Kalisz
+	* Date Created: April 22, 2023
+	* Date Last Modified: April 22, 2026
+	*/
+import java.util.Random;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
-
-}
+	public static int randomInt(int num1, int num2){
+	    Random ran = new Random();
+		return ran.nextInt(num2 - (num2 - num1) + 1)+num1;
+	}
+	
+	public static char randomChar(String str){
+	    Random ran = new Random();
+	    return str.charAt(ran.nextInt(str.length() + 1));
+	}
+	
+	public static int randomEvenNumber(int num1, int num2){
+	    Random ran = new Random();
+	    num1 = num1/2;
+	    num2 = num2/2;
+	    return (ran.nextInt(num2 - (num2 - num1) + 1)+num1 + 1)*2;
+	}
